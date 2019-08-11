def7802a36195e2f5ed7a453eef9441f106cb783
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-11 17:05:01 +0700

    flip word filter (reverse)

--- apps.js
+++ apps.js
@@ -2,19 +2,36 @@ var app = new Vue({
   el:'#app',
 
   // <!-- =========== text filter =========== -->
+  // data: {
+  //   message: 'Filter on VueJS'
+  // },
+  // filters:{
+  //   big(value){
+  //     if(!value) return ''
+  //     value = value.toString()
+  //     return value.toUpperCase()
+  //   },
+  //   small(value){
+  //     if(!value) return ''
+  //     value = value.toString()
+  //     return value.toLowerCase()
+  //   }
+  // }
+
+  // <!-- =========== Flip word filter =========== -->
   data: {
     message: 'Filter on VueJS'
   },
   filters:{
-    big(value){
+    reverse(value){
       if(!value) return ''
       value = value.toString()
-      return value.toUpperCase()
+      return value.split('').reverse().join('')
     },
-    small(value){
+    reverse2(value){
       if(!value) return ''
       value = value.toString()
-      return value.toLowerCase()
+      return value.split(' ').reverse().join(' ')
     }
   }
 
